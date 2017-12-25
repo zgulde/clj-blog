79342d6f13efe0754d3ae0d25b9a921a86ed5c26
Zach Gulde <zachgulde@gmail.com>
2017-12-25 15:42:27 -0600

    Add bootstrap

--- src/clj_blog/views/layout.clj
+++ src/clj_blog/views/layout.clj
@@ -1,9 +1,19 @@
 (ns clj-blog.views.layout
-  (:require [hiccup.page :refer [html5 include-css]]))
+  (:require [hiccup.page :refer [html5 include-css include-js]]))
+
+(def bootstrap-cdn
+  {:css "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"
+   :js "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"})
+(def jquery-cdn "//code.jquery.com/jquery-2.2.4.min.js")
 
 (defn master [title & body]
   (html5
     [:head
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
      [:title title]
-     (include-css "/css/screen.css")]
-    [:body body]))
+     (include-css "/css/screen.css")
+     (include-css (:css bootstrap-cdn))]
+    [:body body
+     (include-js (:js bootstrap-cdn))
+     (include-js jquery-cdn)]))
