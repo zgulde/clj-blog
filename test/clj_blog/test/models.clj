e2e4c4589480fed2ff276434ab15e54712157830
Zach Gulde <zachgulde@gmail.com>
2017-12-27 22:18:01 -0600

    Fix typos

--- test/clj_blog/test/models.clj
+++ test/clj_blog/test/models.clj
@@ -1,13 +1,15 @@
 (ns clj-blog.test.models
-  (:requrie [clj-blog.models.articles :as article]
-            [clj-blog.models.user :as user])
-  (:use clojure.test
-        clj-blog.db.migration
-        clj-blog.db.seeder))
+  (:require [clj-blog.models.article :as article]
+            [clj-blog.models.user :as user]
+            [clj-blog.db.migration :as migration]
+            [clj-blog.db.seeder :as seeder])
+  (:use clojure.test))
 
 (deftest article-model-test
-  (migrate)
+  (migration/migrate)
+  (seeder/seed)
   (testing "insert a record and retrieve it"
-    (let [article {:title "test title" :content "test article content"}
-          new-id (create-article)]))
-  (migrate))
+    ; (let [article {:title "test title" :content "test article content"}
+    ;       new-id (article/create article)])
+    )
+  (migration/migrate))
